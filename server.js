4e4ad17e217e9eeec126c29fd2e5691d0052f46f
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-08 20:50:08 -0700

    heroku added

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 
 
+const notes = require('./db/db.json');
+// const db = require('./db/db');
 
 
 
 
 
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
-app.listen(3001, () =>{
-    console.log(`API server is on port 3001!`);
+app.listen(3003, () =>{
+    console.log(`API server is on port 3003!`);
 });
