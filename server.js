9f72d41cdd774e3c8bbe5446ea2c392a2e7ef4e3
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-09 15:55:39 -0700

    updates to file structure

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
+
+// 1. connect and create a web server
+// 2.  get data
+// 3.  allow users to enter data - post data
+// 4. connect html webpage
+// 5.  connect save feature
+// 6.  add ids
+// 7.  delete function
+
 const express = require('express');
 const PORT = process.env.PORT || 3003;
-const app = express();
-
+var app = express();
 
 const notes = require('./db/db.json');
-// const db = require('./db/db');
 
 
+// remove
+// function queries(query, notesArray) {
+//     let queryResults = notesArray;
 
-
-// function queryFilter(query, noteArray) {
-//     let queries = noteArray;
-//     if (query.text) {
-//         queries = queries.filter(note => note.text === query.text);
-//     }
 //     if (query.title) {
-//         queries = queries.filter(note => note.title === query.title);
-//     }
-//     return queries; 
+//         queryResults = queryResults.filter(note => note.title === query.title);
+//       }
+//     if (query.text) {
+//         queryResults = queryResults.filter(note => note.text === query.text);
+//       }
+//     return queryResults; 
+
 // }
 
+// remove this 
 // app.get('/api/notes', (req, res) => {
-//     let input = notes;
+//     let results = notes;
 //     if (req.query) {
-//         input = queryFilter(req.query, input);
+//         results = queries(req.query, results); 
 //     }
-//     res.json(input);
-//   });
+//     res.json(results); 
+// });
+
 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-});
+})
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`API server is on port ${PORT}!`);
 });
