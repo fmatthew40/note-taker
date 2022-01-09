26553ab58d77f1bfb6d5277f69753e16877ca235
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-08 22:05:59 -0700

    updates to server.js

--- server.js
+++ server.js
@@ -7,14 +7,30 @@ const notes = require('./db/db.json');
 // const db = require('./db/db');
 
 
+function queryFilter(query, noteArray) {
+    let queries = noteArray;
+    if (query.text) {
+        queries = queries.filter(note => note.text === query.text);
+    }
+    if (query.title) {
+        queries = queries.filter(note => note.title === query.title);
+    }
+    return queries; 
+}
 
-
+app.get('/api/notes', (req, res) => {
+    let input = notes;
+    if (req.query) {
+        input = queryFilter(req.query, input);
+    }
+    res.json(input);
+  });
 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.listen(3003, () =>{
+app.listen(PORT, () =>{
     console.log(`API server is on port ${PORT}!`);
 });
