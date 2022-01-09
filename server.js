90b7f5b23aa7cc2fbaf6935af72f37085402ff1b
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-08 22:09:20 -0700

    bug fix

--- server.js
+++ server.js
@@ -7,24 +7,26 @@ const notes = require('./db/db.json');
 // const db = require('./db/db');
 
 
-function queryFilter(query, noteArray) {
-    let queries = noteArray;
-    if (query.text) {
-        queries = queries.filter(note => note.text === query.text);
-    }
-    if (query.title) {
-        queries = queries.filter(note => note.title === query.title);
-    }
-    return queries; 
-}
 
-app.get('/api/notes', (req, res) => {
-    let input = notes;
-    if (req.query) {
-        input = queryFilter(req.query, input);
-    }
-    res.json(input);
-  });
+
+// function queryFilter(query, noteArray) {
+//     let queries = noteArray;
+//     if (query.text) {
+//         queries = queries.filter(note => note.text === query.text);
+//     }
+//     if (query.title) {
+//         queries = queries.filter(note => note.title === query.title);
+//     }
+//     return queries; 
+// }
+
+// app.get('/api/notes', (req, res) => {
+//     let input = notes;
+//     if (req.query) {
+//         input = queryFilter(req.query, input);
+//     }
+//     res.json(input);
+//   });
 
 
 app.get('/api/notes', (req, res) => {
