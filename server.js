f70354285c8c5157b4217d7b29be7a60841c3bbf
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-11 19:14:21 -0700

    delete functionality added, bug fix

--- server.js
+++ server.js
@@ -12,24 +12,16 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-function loadNote() {
-    // fs.readFileSync('./db/db.json');
-//     var content;
-// fs.readFile('db/db.json', function read(err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     content = data;
-// });
-// console.log(content);
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-    };
+function loadNotes() {
+    
+    let noteDB = fs.readFileSync('./db/db.json');
+    return JSON.parse(noteDB);
+};
 
 // gets json file 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    const noteData = loadNotes()
+    res.json(noteData);
 });
  
 // brings up html pages that display on the website
@@ -42,28 +34,31 @@ app.get('/notes', (req, res) => {
   });
 
 app.post('/api/notes', (req, res) => {
-    let addId = req.body;
-    addId.id = Date.now();
+    let note = req.body;
+    note.id = Date.now();
     let noteInfo = fs.readFileSync('./db/db.json');
     let parsedNoteInfo = JSON.parse(noteInfo); 
-    parsedNoteInfo.push(req.body);
+    parsedNoteInfo.push(note);
 
     fs.writeFile(__dirname + "/db/db.json", JSON.stringify(parsedNoteInfo), (err, data) => {
         if (err) throw err;
         res.json(parsedNoteInfo)  
-
-        loadNote();
       }); 
 })
+
+app.delete('/api/notes/:noteId', (req, res) => {
+let notes = loadNotes();
+notes = notes.filter((x) => {
+    return parseInt(x.id) != parseInt(req.params.noteId)
+})
+
+fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err, data) => {
+    if (err) throw err;
+    res.json(notes) 
+  }); 
+});
 
 // port
 app.listen(PORT, () => {
     console.log(`Server is on port ${PORT}!`);
 });
-
-
-
-
-
-
-
